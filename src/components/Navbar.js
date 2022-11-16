e17dcaa99dde200f67fda1a07426b90acb9ce286
^Kamil^ <84046930+Kamilismayilzade@users.noreply.github.com>
2022-11-16 23:51:55 +0100

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,7 +8,7 @@ import Logo from '../assets/logo.png'
 const Navbar = () => {
   return (
 
-    <Stack direction='row' justifyContent='space-between' backgroundColor='#F2EFC7' boxShadow='10px 10px 8px #888888;' sx={{gap: {sm: '2rem', xs: '1rem'}, mt: {sm: '0rem', xs: '.5rem'}, justifyContent: 'none'}} >
+    <Stack direction='row' justifyContent='space-between' backgroundColor='#F8F9F9' boxShadow='10px 10px 8px #888888;' sx={{gap: {sm: '2rem', xs: '1rem'}, mt: {sm: '0rem', xs: '0rem'}, py: {sm: '1rem', xs: '1.2rem'}, justifyContent: 'none'}} >
 
       <Link to='/'>
         <img src={Logo} alt='logo' style={{width: '3rem', height: '3rem', margin: '0 1rem'}} />
